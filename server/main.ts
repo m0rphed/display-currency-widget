baf9a363cf437391502538e4e8e92838cb44935c
Vic Khov <victorkhovtko@gmail.com>
2024-07-28 22:09:38 +0300

    formatting

--- server/main.ts
+++ server/main.ts
@@ -1,13 +1,13 @@
 import { Hono } from "hono";
-import { serveStatic } from "hono/deno"
+import { serveStatic } from "hono/deno";
 import { cors } from "hono/cors";
 
 const app = new Hono();
 app.use("/*", cors());
 
 // serve JSON folder with currency names
+app.use("/jsons/*", serveStatic({ root: "./" }));
 
-app.use("/jsons/*", serveStatic({root: "./"}))
 Deno.serve(app.fetch);
 
-export default app
+export default app;
