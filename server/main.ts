55763e84fcaf9d71988c6e90f1a93ebbfd7400e5
Vic Khov <victorkhovtko@gmail.com>
2024-07-28 22:09:19 +0300

    added JSON files to use for fake exchange rates

--- server/main.ts
+++ server/main.ts
@@ -1,8 +1,13 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/deno"
+import { cors } from "hono/cors";
 
 const app = new Hono();
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+app.use("/*", cors());
 
+// serve JSON folder with currency names
+
+app.use("/jsons/*", serveStatic({root: "./"}))
 Deno.serve(app.fetch);
+
+export default app
