a476e44feb62d302adc2377fdf08d3f7cfd055cf
Vic Khov <victorkhovtko@gmail.com>
2024-07-30 02:58:27 +0300

    finished card-row component skeleton

--- client/src/components/card-row.tsx
+++ client/src/components/card-row.tsx
@@ -1,16 +1,87 @@
-import {useForm} from "react-hook-form"
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form.tsx";
+import { Input } from "@/components/ui/input.tsx";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select.tsx";
 
 const formSchema = z.object({
-    amount: z.number().positive().multipleOf(0.01),
-    currency: z.string(),
-})
+  amount: z.number().positive().multipleOf(0.01),
+  currency: z.string(),
+});
 
 const CardRow = () => {
-    const form = useForm<z.infer<typeof formSchema>>(
-    )
-  return <div>CardRow</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      amount: undefined,
+      currency: undefined,
+    },
+  });
+
+  return (
+    <div className="flex gap-5">
+      <Form {...form}>
+        <form className="space-y-8">
+          <FormField
+            control={form.control}
+            name="amount"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Amount</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="0"
+                    type="number"
+                    pattern="[0-9]*"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+        <form className="space-y-8">
+          <FormField
+            control={form.control}
+            name="currency"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Amount</FormLabel>
+                <FormControl>
+                  <Select {...field}>
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Theme" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="light">Light</SelectItem>
+                      <SelectItem value="dark">Dark</SelectItem>
+                      <SelectItem value="system">System</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default CardRow;
