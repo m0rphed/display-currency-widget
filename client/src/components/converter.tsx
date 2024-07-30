f456543cb6aa9345f6c6f9b1316d4fe29afcfe89
Vic Khov <victorkhovtko@gmail.com>
2024-07-30 03:24:23 +0300

    added fetching data function

--- client/src/components/converter.tsx
+++ client/src/components/converter.tsx
@@ -1,4 +1,4 @@
-import CardRow from "./card-row.tsx";
+import "jsr:@std/dotenv/load";
 import {
   Card,
   CardContent,
@@ -6,11 +6,30 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card.tsx";
+import CardRow from "./card-row.tsx";
+import useSWR from "swr";
 
-import "jsr:@std/dotenv/load";
 const BASE_URL = Deno.env.get("VITE_BE_ENV");
 
 const Converter = () => {
+  const {
+    data: namesData,
+    error: namesError,
+    isLoading: isNamesLoading,
+  } = useSWR(`${BASE_URL}/names`, (url) => {
+    fetch(url).then((res) => res.json());
+  });
+
+  const {
+    data: currenciesData,
+    error: currenciesError,
+    isLoading: isCurrenciesLoading,
+  } = useSWR(`${BASE_URL}/currencies`, (url) => {
+    fetch(url).then((res) => res.json());
+  });
+
+  console.log(currenciesData, namesData, "DATA");
+
   return (
     <Card className="max-width-300px">
       <CardHeader className="border-b-2 p-2 m-2">
