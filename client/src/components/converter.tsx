91ea6cd4ed1b69aafe1d618a1bb9cbfe087f1b02
Vic Khov <victorkhovtko@gmail.com>
2024-08-03 01:49:44 +0300

    fixed: env variable management issue with Deno resolved by using "Vite-way"

--- client/src/components/converter.tsx
+++ client/src/components/converter.tsx
@@ -1,4 +1,3 @@
-import "jsr:@std/dotenv/load";
 import {
   Card,
   CardContent,
@@ -9,7 +8,15 @@ import {
 import CardRow from "./card-row.tsx";
 import useSWR from "swr";
 
-const BASE_URL = Deno.env.get("VITE_BE_ENV");
+
+// [WARNING] Deno v1.45.4
+// Because of current state of "Deno + Vite"
+// -> `import.meta.env.VITE_VARIABLE_NAME` is used for retrieving .env variables,
+// instead of `Deno.env.get("VAR_NAME")`
+// - which would be marked as error by Deno LSP,
+// because Deno by itself does not define `import.meta.env`
+const BASE_URL = import.meta.env.VITE_BE_ENV;
+// console.log(`\n[DEBUG]: BASE_URL of backend is '${BASE_URL}'`);
 
 const Converter = () => {
   const {
