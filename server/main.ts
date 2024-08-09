8010a1957f0960da4d85cd68bf916f9c7ee25d5a
Vic Khov <victorkhovtko@gmail.com>
2024-08-09 21:38:36 +0300

    used caching strategy for endpoint "/currencies"

--- server/main.ts
+++ server/main.ts
@@ -1,12 +1,25 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/deno";
+import { cache } from "hono/cache";
 import { cors } from "hono/cors";
+
+const CACHE_DURATION_SECONDS = 3600; // cache for 1 hour (= 3600 seconds)
 
 const app = new Hono();
 app.use("/*", cors());
 
 // serve JSON folder with currency names
 app.use("/jsons/*", serveStatic({ root: "./" }));
+
+// configure caching
+app.use(
+  "/currencies",
+  cache({
+    cacheName: "currency-rates",
+    cacheControl: `max-age=${CACHE_DURATION_SECONDS}`,
+    wait: true, // needed for Deno.js runtime
+  })
+);
 
 app.get("/currencies", (c) => {
   return c.redirect("/jsons/currencies.json");
