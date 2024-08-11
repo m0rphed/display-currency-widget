d07e755277a5ff8f94e1926b8a8df4ba8a3ebcdc
Vic Khov <victorkhovtko@gmail.com>
2024-08-11 19:04:40 +0300

    upd: error component

--- client/src/components/error-component.tsx
+++ client/src/components/error-component.tsx
@@ -1,9 +1,9 @@
 const ErrorComponent = ({ message }: { message: string }) => {
   return (
-    <div className="flex bg-gray-100 w-[65vw]">
+    <div className="flex bg-gray-100 rounded-lg">
       <div className="bg-white p-8 rounded-lg shadow-md text-center">
         <h2 className="text-3xl font-bold text-red-600 mb-4">
-          Oops, something went wrong, please try again or contact support or something idk...
+          Oops, something went wrong, please try again or something idk...
         </h2>
         <p className="text-gray-800 mb-6">{message}</p>
       </div>
